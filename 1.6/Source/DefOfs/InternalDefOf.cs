58cb142d96a0b71b19756185e3ee1f93cc761dd8
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-05-21 10:18:50 +0200

    Using VE template

---
+++ 1.6/Source/DefOfs/InternalDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+namespace VanillaGravshipExpanded2
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+		static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+		
+	}
+}
---
+++ 1.6/Source/Harmony/HarmonyInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+namespace VanillaGravshipExpanded2
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.VanillaGravshipExpanded2");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
