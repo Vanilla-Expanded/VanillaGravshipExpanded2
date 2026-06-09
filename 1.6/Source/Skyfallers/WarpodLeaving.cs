30596fd4f818fa66c6c29ca9e76dbda828706506
Taranchuk <nfhfyxer@gmail.com>
2026-06-10 00:08:18 +0300

    warpod tweaks

--- 1.6/Source/Skyfallers/WarpodLeaving.cs
+++ 1.6/Source/Skyfallers/WarpodLeaving.cs
@@ -26,18 +26,45 @@ namespace VanillaGravshipExpanded2
             Scribe_Defs.Look(ref worldObjectDef, "worldObjectDef");
         }
 
+        private float CurrentU
+        {
+            get
+            {
+                var t = (float)ticksToImpact / LeaveMapAfterTicks;
+                return 3f * t * t * t * t;
+            }
+        }
+
+        private Vector3 GetFlightOffset(float u)
+        {
+            if (u <= 1f)
+            {
+                return new Vector3(-50f * u * u, 0f, 100f * u - 50f * u * u);
+            }
+            return new Vector3(-100f * u + 50f, 0f, 50f);
+        }
+
+        private float GetFlightAngle(float u)
+        {
+            var dx = -100f;
+            var dz = 0f;
+            if (u <= 1f)
+            {
+                dx = -100f * u;
+                dz = 100f - 100f * u;
+            }
+            return Mathf.Atan2(dx, dz) * Mathf.Rad2Deg;
+        }
+
         public override Vector3 DrawPos
         {
             get
             {
                 var pos = Position.ToVector3Shifted();
-                var progress = (float)ticksToImpact / LeaveMapAfterTicks;
-                if (progress > 0f && progress < 1f)
+                if (ticksToImpact > 0)
                 {
                     pos.y = Altitudes.AltitudeFor(AltitudeLayer.Skyfaller);
-
-                    pos.x += -50f * progress * progress;
-                    pos.z += 100f * progress - 50f * progress * progress;
+                    pos += GetFlightOffset(CurrentU);
                 }
                 return pos;
             }
@@ -46,20 +73,18 @@ namespace VanillaGravshipExpanded2
         public override void DrawAt(Vector3 drawLoc, bool flip = false)
         {
             var angle = 0f;
-            var progress = (float)ticksToImpact / LeaveMapAfterTicks;
-            if (progress > 0f && progress < 1f)
+            var t = 0f;
+            if (ticksToImpact > 0)
             {
-
-                var dx = -100f * progress;
-                var dz = 100f - 100f * progress;
-                angle = Mathf.Atan2(dx, dz) * Mathf.Rad2Deg;
+                t = (float)ticksToImpact / LeaveMapAfterTicks;
+                angle = GetFlightAngle(CurrentU);
             }
 
             Graphic.Draw(drawLoc, flip ? Rotation.Opposite : Rotation, this, angle);
 
-            if (progress > 0f && progress < 1f)
+            if (t > 0f)
             {
-                var flameScale = new Vector3(1.2f, 1f, 2.2f * progress);
+                var flameScale = new Vector3(1.2f, 1f, 2.2f * t);
                 var nozzleOffsetDist = 0.2f;
                 var flameOffset = Quaternion.Euler(0f, angle, 0f) * Vector3.back * (nozzleOffsetDist + (flameScale.z * 0.5f));
                 flameOffset.y -= 0.1f;
@@ -72,19 +97,23 @@ namespace VanillaGravshipExpanded2
         public override void Tick()
         {
             base.Tick();
+            if (Spawned)
+            {
+                if (!new IntVec3(DrawPos).InBounds(Map))
+                {
+                    LeaveMap();
+                    return;
+                }
+            }
             if (Spawned && ticksToImpact > 0 && ticksToImpact < LeaveMapAfterTicks)
             {
-                var progress = (float)ticksToImpact / LeaveMapAfterTicks;
-
-                var dx = -100f * progress;
-                var dz = 100f - 100f * progress;
-                var angle = Mathf.Atan2(dx, dz) * Mathf.Rad2Deg;
-
                 var pos = DrawPos;
+                var angle = GetFlightAngle(CurrentU);
                 var exhaustOffset = Quaternion.Euler(0f, angle, 0f) * Vector3.back * 0.8f;
 
                 FleckMaker.ThrowSmoke(pos + exhaustOffset, Map, 1.5f);
-                if (this.IsHashIntervalTick(4))
+                var t = (float)ticksToImpact / LeaveMapAfterTicks;
+                if (Rand.Chance(Mathf.Lerp(0.25f, 1f, t)))
                 {
                     FleckMaker.ThrowHeatGlow((pos + exhaustOffset).ToIntVec3(), Map, 1.5f);
                 }
