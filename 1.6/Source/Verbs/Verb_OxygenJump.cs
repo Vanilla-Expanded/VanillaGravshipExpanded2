ff3c4294e5a6f8986509a6fde94c6df2bdce3199
SokyranTheDragon <exploder1531@gmail.com>
2026-06-14 21:38:19 +0200

    Astrorig uses our own oxygen pack stat for range

--- 1.6/Source/Verbs/Verb_OxygenJump.cs
+++ 1.6/Source/Verbs/Verb_OxygenJump.cs
@@ -6,6 +6,22 @@ public class Verb_OxygenJump : Verb_Jump, IOxygenVerb
 {
     public CompApparelVerbOwner_Oxygen VerbOwner_OxygenCompSource => DirectOwner as CompApparelVerbOwner_Oxygen;
 
+    public override float EffectiveRange
+    {
+        get
+        {
+            if (cachedEffectiveRange < 0f)
+            {
+                var equipment = EquipmentSource;
+                if (equipment != null && verbProps.rangeStat != null)
+                    cachedEffectiveRange = equipment.GetStatValue(verbProps.rangeStat);
+                else
+                    cachedEffectiveRange = base.EffectiveRange;
+            }
+            return cachedEffectiveRange;
+        }
+    }
+
     public override bool TryCastShot()
     {
         var comp = VerbOwner_OxygenCompSource;
