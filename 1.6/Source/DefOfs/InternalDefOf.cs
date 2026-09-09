563b37e175657bfd7a64e25042e15348c986e92e
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-09-09 16:59:03 +0200

    Patched CocoonInfestationUtility_GetCocoonsToSpawn

--- 1.6/Source/DefOfs/InternalDefOf.cs
+++ 1.6/Source/DefOfs/InternalDefOf.cs
@@ -37,6 +37,7 @@ namespace VanillaGravshipExpanded2
         public static ThingDef VFEI2_VGE_Subcreeper;
         [MayRequire("OskarPotocki.VFE.Insectoid2")]
         public static ThingDef VFEI2_Siegeworm;
+        public static ThingDef CocoonMegascarab;
 
         public static PawnKindDef VGE_ExowormKnot;
         public static PawnKindDef VGE_Exopede;
---
+++ 1.6/Source/Harmony/CocoonInfestationUtility_GetCocoonsToSpawn.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.Sound;
+
+namespace VanillaGravshipExpanded2
+{
+    [HarmonyPatch(typeof(CocoonInfestationUtility), nameof(CocoonInfestationUtility.GetCocoonsToSpawn))]
+    public static class VanillaGravshipExpanded2_CocoonInfestationUtility_GetCocoonsToSpawn_Patch
+    {
+        public static IEnumerable<ThingDef> Postfix(IEnumerable<ThingDef> __result)
+        {
+            foreach (ThingDef g in __result)
+            {
+                if (g == InternalDefOf.VGE_ExowormCocoon)
+                {
+                    yield return InternalDefOf.CocoonMegascarab;
+                }
+                else { yield return g; }
+                
+            }
+        }
+    }
+}
